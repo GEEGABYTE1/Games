64ec6f5d806071796c0563f50b728e69615e788e
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-11-05 21:59:53 -0400

    Completion with string formats

--- rock_paper_scissors.go
+++ rock_paper_scissors.go
@@ -26,24 +26,24 @@ func main() {
 	fmt.Scan(&user_input)
 
 	if user_input == computer_input {
-		fmt.Println("You have tied with the computer")
+		fmt.Println("You have tied with the computer!")
 	} else if user_input == 1 && computer_input == 3 {
-		fmt.Println("You have chosen rock and the computer chose scissors")
+		fmt.Println("You have chosen rock and the computer chose scissors!")
 		fmt.Println("Congratulations, you have won")
 	} else if user_input == 3 && computer_input == 1 {
-		fmt.Println("You have chosen scissors and the computer chose rock")
+		fmt.Println("You have chosen scissors and the computer chose rock!")
 		fmt.Println("Sadly, you have lost")
 	} else if user_input == 1 && computer_input == 2 {
-		fmt.Println("You have chosen rock and the computer chose paper")
+		fmt.Println("You have chosen rock and the computer chose paper!")
 		fmt.Println("Sadly, you have lost")
 	} else if user_input == 2 && computer_input == 1 {
-		fmt.Println("You have chosen paper and the computer chose rock")
+		fmt.Println("You have chosen paper and the computer chose rock!")
 		fmt.Println("Congratulations, you have won")
 	} else if user_input == 2 && computer_input == 3 {
-		fmt.Println("You have chosen paper and the computer chose scissors")
+		fmt.Println("You have chosen paper and the computer chose scissors!")
 		fmt.Println("Sadly, you have lost")
 	} else if user_input == 3 && computer_input == 2 {
-		fmt.Println("You have chosen scissors and the computer chose paper")
+		fmt.Println("You have chosen scissors and the computer chose paper!")
 		fmt.Println("Congratulations, you have won")
 	}
 }
